9bd93e63a34960ed7fe2dd11c7037faf34d95dd8
Means88 <means88.x@gmail.com>
2019-08-19 17:13:29 +0800

    bug: reproduce bug of meta

--- __tests__/building.test.js
+++ __tests__/building.test.js
@@ -6,29 +6,36 @@ describe('Building from scratch', () => {
     clearOutput();
   });
 
-  it('should write a valid file', () => {
-    let sketch = new Sketch();
-    const page = new Page({
-      name: 'My Page'
-    });
-    const artboard = new Artboard({
-      frame: {
-        width: 1024,
-        height: 768
-      },
-      name: 'My Artboard'
-    });
-    const text = new Text({
-      name: 'My Text',
-      string: 'Test',
-      fontSize: 24
-    });
-    artboard.addLayer( text );
-    sketch.addPage( page );
-    sketch.addArtboard(page.getID(), artboard);
-    const path = `${process.cwd()}/__tests__/__output/output2.sketch`;
-    sketch.build(path).then((path) => {
-      expect(fileExists(path)).toBeTruthy();
-    });
+  it('should write a valid file', async () => {
+    await (async () => {
+      let sketch = new Sketch();
+      const page = new Page({
+        name: 'My Page'
+      });
+      const artboard = new Artboard({
+        frame: {
+          width: 1024,
+          height: 768
+        },
+        name: 'My Artboard'
+      });
+      const text = new Text({
+        name: 'My Text',
+        string: 'Test',
+        fontSize: 24
+      });
+      artboard.addLayer( text );
+      sketch.addPage( page );
+      sketch.addArtboard(page.getID(), artboard);
+      const path = `${process.cwd()}/__tests__/__output/output2.sketch`;
+      const outputPath = await sketch.build(path);
+      expect(fileExists(outputPath)).toBeTruthy();
+    })();
+
+    // another sketch should have correct meta
+    await (async () => {
+      const { meta } = new Sketch();
+      expect(Object.keys(meta.pagesAndArtboards || {}).length).toEqual(0);
+    })();
   });
 });
