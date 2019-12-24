78098cb2a7166d20c590e14fc0188f563ff9e572
Andrey Chalkin <andrey.chalkin@sidenis.com>
2019-12-24 15:57:03 +0700

    chore: update examples

--- __examples__/generating-sketch-from-style-dictionary/sketch/index.js
+++ __examples__/generating-sketch-from-style-dictionary/sketch/index.js
@@ -1,42 +1,39 @@
 const { Sketch } = require('sketch-constructor');
-
 const ColorPage = require('./ColorPage');
 
-module.exports = function(dictionary, platform) {
+module.exports = function BuildSketchWithStyles(dictionary) {
   // Create a new sketch instance
-  var sketch = new Sketch();
+  const sketch = new Sketch();
 
   // Add shared styles for all background colors
   dictionary.allProperties
     .filter(prop => {
-      return prop.attributes.category === 'color' &&
-             prop.attributes.type === 'background'
+      return prop.attributes.category === 'color' && prop.attributes.type === 'background';
     })
     .forEach(prop => {
       sketch.addLayerStyle({
         name: prop.path.join(' / '),
-        fills: [{
-          color: prop.value
-        }]
+        fills: [
+          {
+            color: prop.value,
+          },
+        ],
       });
     });
 
   // Add typography styles
-  Object.keys(dictionary.properties.component.typography)
-    .forEach(key => {
-      let font = dictionary.properties.component.typography[key];
-      sketch.addTextStyle({
-        name: key,
-        fontSize: font['font-size'].value,
-        color: font['color'].value
-      })
+  Object.keys(dictionary.properties.component.typography).forEach(key => {
+    const font = dictionary.properties.component.typography[key];
+    sketch.addTextStyle({
+      name: key,
+      fontSize: font['font-size'].value,
+      color: font.color.value,
     });
+  });
 
   // Add a custom color page
-  sketch.addPage(
-    new ColorPage(dictionary)
-  );
+  sketch.addPage(new ColorPage(dictionary));
 
   // Build the sketch file
   sketch.build('./dist/style-dictionary.sketch');
-}
+};
