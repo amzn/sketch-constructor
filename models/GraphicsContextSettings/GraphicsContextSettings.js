7441d48cf17d3873ec04d8251a807d31a2a958be
Andrey Chalkin <L2jLiga@gmail.com>
2020-08-10 21:41:45 +0700

    fix: opacity = 1 when args.opacity is zero (falsy value)

--- models/GraphicsContextSettings/GraphicsContextSettings.js
+++ models/GraphicsContextSettings/GraphicsContextSettings.js
@@ -40,7 +40,7 @@ class GraphicsContextSettings {
     } else {
       Object.assign(this, GraphicsContextSettings.Model, args, {
         blendMode: blendModeMap[args.blendMode || 'normal'],
-        opacity: args.opacity || 1,
+        opacity: Number.isFinite(args.opacity) ? args.opacity : 1,
       });
     }
     return this;
